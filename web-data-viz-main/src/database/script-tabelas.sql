96b277aa5e35fe1391cb5135e7ce475c69044e54
EnzoBizeli <enzo.bizeli@sptech.school>
2024-05-17 14:57:39 -0300

    Modelagem e script feitos

--- web-data-viz-main/src/database/script-tabelas.sql
+++ web-data-viz-main/src/database/script-tabelas.sql
@@ -6,52 +6,21 @@
 comandos para mysql server
 */
 
-CREATE DATABASE ProjetoIndividual;
-USE ProjetoIndividual;
+create database ProjetoIndividual;
+use ProjetoIndividual;
 
--- CREATE TABLE empresa (
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	razao_social VARCHAR(50),
--- 	cnpj CHAR(14)
--- );
-
-CREATE TABLE usuario (
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(45),
-	email VARCHAR(45),
-	senha VARCHAR(45)
-	confirmacaoSenha VARCHAR(45)
-	);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(45),
+senha varchar(45),
+confirmacao_senha varchar(45)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table quiz (
+tentativa int primary key auto_increment,
+fkUsuario int, 
+pontuacao int,
+constraint fkQuizUsuario foreign key (fkUsuario)
+	references usuario(idUsuario)
 );
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
